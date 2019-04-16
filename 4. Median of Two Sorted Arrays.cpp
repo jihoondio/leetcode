9bcde1cd239eda720336323ab632dad3af0aed35
jihoondio <jihoondio@gmail.com>
2019-04-16 12:59:59 +0900

    Add files via upload

---
+++ 4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+		nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+
+		auto & nums = nums1;
+		sort(nums.begin(), nums.end());
+
+		if (nums.size() % 2 == 1)
+		{
+			return (double)nums[nums.size() / 2];
+		}
+		else
+		{
+			int first = (nums.size() - 1) / 2;
+			int second = first + 1;
+
+			return (double)((nums[first] + nums[second]) / 2.0f);
+		}
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> nums1 = { 1,2 };
+	vector<int> nums2 = { 3,4 };
+	s.findMedianSortedArrays(nums1, nums2);
+}

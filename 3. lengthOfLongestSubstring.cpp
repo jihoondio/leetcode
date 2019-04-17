bcc98369b413b57723cf4f0309de42fdb8ac382f
jihoondio <jihoondio@gmail.com>
2019-04-17 11:20:23 +0900

    Add files via upload

---
+++ 3. lengthOfLongestSubstring.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+
+		if (s.length() == 0 || s.length() == 1) return s.length();
+
+		int max_len = 0;				
+		string result;
+		for (int i = 0; i < s.size(); i++)
+		{
+			char ch = s[i];
+
+			auto index = result.find(ch);
+			if (index != string::npos)
+			{
+				if (result.size() > max_len)
+				{
+					max_len = result.size();
+				}
+				result = result.substr(index + 1, i - index + 1);
+				result.append(1, ch);
+			}
+			else
+			{
+				result.append(1, ch);
+				if (result.size() > max_len)
+				{
+					max_len = result.size();
+				}
+			}
+		}
+
+		return max_len;
+	}
+};
+
+int main()
+{
+	Solution s;
+    std::cout << s.lengthOfLongestSubstring(string("au"));
+}

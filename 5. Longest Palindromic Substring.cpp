197697d6bf75ce9a812a9e15bea9f40e0808fabf
jihoondio <jihoondio@gmail.com>
2019-05-07 15:36:12 +0900

    Add files via upload

---
+++ 5. Longest Palindromic Substring.cpp
@@ -0,0 +1,38 @@
+﻿class Solution {
+public:
+	string longestPalindrome(string s) {
+
+		string result;
+
+		if (s.empty()) return "";
+		if (s.size() == 1) return s;
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			for (int j = s.length() - 1; j >= i; j--)
+			{
+				if (j - i + 1 < result.length()) break;
+				
+				int f = i;
+				int b = j;
+								
+				while (f <= b)
+				{
+					if (s[f] != s[b]) break;
+					f++;
+					b--;
+				}
+
+				if (f > b)
+				{
+					if (j - i + 1 > result.length())
+					{
+						result = s.substr(i, j - i + 1);
+					}
+				}
+			}
+		}
+
+		return result;
+	}
+};

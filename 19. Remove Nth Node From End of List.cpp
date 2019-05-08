37652ce498a891f08f6b21a9713871988204799e
jihoondio <jihoondio@gmail.com>
2019-05-08 17:21:11 +0900

    Add files via upload

---
+++ 19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,32 @@
+﻿struct ListNode {
+	int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		
+		ListNode  list(0);
+		list.next = head;
+
+		ListNode* p1 = &list;
+		ListNode* p2 = &list;
+
+		for (int i = 0; i <= n && p2 != NULL; i++)
+		{
+			p2 = p2->next;
+		}
+
+		while (p2 != NULL)
+		{
+			p1 = p1->next;
+			p2 = p2->next;
+		}
+		
+		p1->next = p1->next->next;
+
+		return list.next;
+	}
+};

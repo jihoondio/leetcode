4b7f28a50e9d2873b961f948c628b6f7b663efe6
jihoondio <jihoondio@gmail.com>
2021-02-19 15:30:45 +0900

    Create 1748. sumOfUnique.cpp

---
+++ 1748. sumOfUnique.cpp
@@ -0,0 +1,19 @@
+int sumOfUnique(int* nums, int numsSize){
+    int hashes[100] = {0,};
+    int sum = 0;
+    int dup_sum = 0;
+    for ( int i = 0; i < numsSize; i++ )
+    {
+        int num = nums[i];
+        hashes[num - 1]++;
+        sum += num;
+        if ( hashes[num - 1] > 1 ) {
+            dup_sum += num;
+            if ( hashes[num - 1] == 2 ) {
+                dup_sum += num;
+            }
+        }
+    }
+    
+    return sum - dup_sum;
+}
